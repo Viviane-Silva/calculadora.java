68f59564f47e8d8a653a4199d1e2e95302ad639f
Viviane-Silva <viviane.leite.silva@gmail.com>
2021-05-06 10:27:56 -0300

    Calculadora devisualizações de anuncios sobre investimento.

---
+++ src/Calculadora.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+
+
+public class Calculadora {
+
+    public static void main(String[] args){
+
+     int valorInvestido;
+     int anuncioOriginal;
+     int anuncioCompartilhado;
+     float resultado;
+
+     //entrada
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o valor de investimento");
+        valorInvestido = input.nextInt();
+
+        //a cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais.
+        //cada compartilhamento nas redes sociais gera 40 novas visualizações.
+        //30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.
+
+        anuncioOriginal = 30; //qtd.visualizações
+        anuncioCompartilhado = 120; //qtd.visualizações
+
+
+        resultado =  (anuncioOriginal + anuncioCompartilhado) * valorInvestido;
+
+        int numeroResultado = Math.round(resultado);
+        System.out.println(numeroResultado+" Visualizações aproximadas com o valor investido!");
+
+
+
+    }
+}
